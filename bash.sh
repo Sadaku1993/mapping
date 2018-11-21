9b7d58de78214381798dcacac14629a9e6859da1
Sadaku1993 <36195395+Sadaku1993@users.noreply.github.com>
2018-11-21 19:36:19 +0900

    update

---
+++ bash.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Hello World"
+
+# make data directory
+mkdir data
+cd data
+
+mkdir pointcloud
+mkdir gyro_odometry
+mkdir normal_estimation
+mkdir gicp
+mkdir g2o
+mkdir map
+
+exit 0
