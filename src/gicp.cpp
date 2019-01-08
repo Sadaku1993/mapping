82daa07a26ea7059f5fe70f5c4c48799ae080ebd
Sadaku1993 <36195395+Sadaku1993@users.noreply.github.com>
2019-01-09 00:20:24 +0900

    update

--- src/gicp.cpp
+++ src/gicp.cpp
@@ -66,11 +66,11 @@ void Gicp<T_p>::gicp(typename pcl::PointCloud<T_p>::Ptr& source_cloud,
                      typename pcl::PointCloud<T_p>::Ptr& target_cloud, 
                      Eigen::Matrix4f& transformation_matrix)
 {
-	pcl::GeneralizedIterativeClosestPoint<T_p, T_p> gicp; 
-	gicp.setMaxCorrespondenceDistance (0.7);
-	gicp.setMaximumIterations (100);        
-	gicp.setTransformationEpsilon (1e-8);   
-	gicp.setEuclideanFitnessEpsilon (1e-8);
+    pcl::GeneralizedIterativeClosestPoint<T_p, T_p> gicp; 
+    gicp.setMaxCorrespondenceDistance (0.7);
+    gicp.setMaximumIterations (100);        
+    gicp.setTransformationEpsilon (1e-8);   
+    gicp.setEuclideanFitnessEpsilon (1e-8);
     gicp.setInputSource(source_cloud);
     gicp.setInputTarget(target_cloud);
     pcl::PointCloud<T_p> Final;
